3301f135e0b87da2686a4e4813c8649073c0f6c9
Saidislombek <60057538+Saidislombek@users.noreply.github.com>
2021-02-06 14:36:40 +0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,19 @@
   });
 
 
+//  Translate languages 
+$('.ru').on('click', function() {
+  $('[data-ru]').each(function() {
+    $(this).text($(this).attr('data-ru'));
+  }); 
+});
+
+$('.uz').on('click', function() {
+  $('[data-uz]').each(function() {
+    $(this).text($(this).attr('data-uz'));
+  }); 
+});
+
   // Smooth scroll for the navigation menu and links with .scrollto classes
   var scrolltoOffset = $('#header').outerHeight() - 1;
   $(document).on('click', '.nav-menu a, .mobile-nav a, .scrollto', function(e) {
